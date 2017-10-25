e114d69c9a6ca0dc739bda35532117bf9b736fdf
Nathalie Zhang <nathalie@MacBook-Pro-de-Nathalie.local>
2017-10-25 23:00:47 +0200

    Add stones + clean

--- app/scripts/App.js
+++ app/scripts/App.js
@@ -15,8 +15,8 @@ export default class App {
     	document.body.appendChild( this.container );
 
         this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 550 );
-        this.camera.position.y = 35;
-        this.camera.position.z = 100;
+        this.camera.position.y = 40;
+        this.camera.position.z = 80;
 
     	this.scene = new THREE.Scene();
 
@@ -27,27 +27,26 @@ export default class App {
       this.planeWidth = 300;
       this.planeHeight = 300;
 
-      let plain = new THREE.PlaneGeometry(this.planeWidth, this.planeHeight, 100, 100);
-      // console.log(plain);
+      let plane = new THREE.PlaneGeometry(this.planeWidth, this.planeHeight, 100, 100);
       // console.log(plain.vertices);
-      let plainTextureLoader = new THREE.TextureLoader();
-      let plainTexture = plainTextureLoader.load(PlainTexture);
+      let planeTextureLoader = new THREE.TextureLoader();
+      let planeTexture = planeTextureLoader.load(PlainTexture);
 
-      plainTexture.wrapS = THREE.RepeatWrapping;
-      plainTexture.wrapT = THREE.RepeatWrapping;
+      planeTexture.wrapS = THREE.RepeatWrapping;
+      planeTexture.wrapT = THREE.RepeatWrapping;
 
-      let plainMaterial = new THREE.MeshPhongMaterial(
+      let planeMaterial = new THREE.MeshPhongMaterial(
         {
           color: 0x30a280,
           emissive: 0x002a25,
           // wireframe: true,
-          map: plainTexture,
-          displacementMap: plainTexture,
-          displacementScale: 20
+          map: planeTexture,
+          displacementMap: planeTexture,
+          displacementScale: 30
         }
       );
 
-      this.meshPlain = new THREE.Mesh( plain, plainMaterial );
+      this.meshPlain = new THREE.Mesh( plane, planeMaterial );
       this.meshPlain.rotation.x = - Math.PI / 2;
       this.scene.add( this.meshPlain );
 
@@ -57,7 +56,7 @@ export default class App {
         Fog
         */
       this.scene.background = new THREE.Color( 0xe9f8ff );
-      this.scene.fog = new THREE.FogExp2 (0xe9f8ff, 0.008);
+      this.scene.fog = new THREE.FogExp2 (0xe9f8ff, 0.01);
 
       /**
         Light
@@ -75,7 +74,7 @@ export default class App {
     	this.container.appendChild( this.renderer.domElement );
 
       /**
-        Debug controls after camera and renderer
+        Debug controls : after camera and renderer
         */
       let controls;
       controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
@@ -86,6 +85,9 @@ export default class App {
         this.renderer.animate( this.render.bind(this) );
     }
 
+    /**
+      Retrieve trees positions with the plane coordinates
+      */
     treesPosition() {
 
       // Load the image
@@ -95,6 +97,9 @@ export default class App {
 
     }
 
+    /**
+      CONVERT CANVAS POSITIONS TO PLANE POSITIONS
+      */
     onLoadImage (event) {
       // console.log(this.texture);
       var positions = [];
@@ -107,16 +112,18 @@ export default class App {
       canvas.height = 256;
 
       // Show the canvas
-      canvas.style.top = '0';
-      canvas.style.position = 'absolute';
-      document.body.appendChild(canvas);
+      // canvas.style.top = '0';
+      // canvas.style.position = 'absolute';
+      // document.body.appendChild(canvas);
 
       ctx.drawImage(this.texture, 0, 0, 256, 256);
 
-      // Retrieve pixels values
+      // Retrieve pixels values of the image
       let textureData = ctx.getImageData(0, 0, canvas.width, canvas.height);
       let data = textureData.data; // All values
       let pixels = [];
+      let scaleZ = 0.095; // Scale of the position z
+      let ratio = this.planeWidth / canvas.width;
 
       for (let i = 0, c = data.length; i < c; i+= 4) {
         pixels.push(
@@ -125,10 +132,6 @@ export default class App {
           }
         )
       }
-      // console.log(pixels);
-
-
-      /** CONVERT CANVAS POSITIONS TO PLANE POSITIONS **/
 
       // Convert canvas x, y positions to Plane positions
       // First loop for coordinate y & second for x : from left to right, from top to bottom
@@ -138,13 +141,13 @@ export default class App {
           if (i <= canvas.height / 2) { // i < 128
             if (j <= canvas.width / 2) { // j < 128
               positions.push({
-                x: (-this.planeWidth / 2) + (this.planeWidth / canvas.width) * j,
-                y: (-this.planeWidth / 2) + (this.planeWidth / canvas.width) * i
+                x: (-this.planeWidth / 2) + ratio * j,
+                y: (-this.planeWidth / 2) + ratio * i
               });
             } else { // j > 128
               positions.push({
-                x: (j - canvas.width / 2) * (this.planeWidth / canvas.width),
-                y: (-this.planeWidth / 2) + (this.planeWidth / canvas.width) * i
+                x: (j - canvas.width / 2) * ratio,
+                y: (-this.planeWidth / 2) + ratio * i
               });
             }
           }
@@ -152,13 +155,13 @@ export default class App {
           else if (i >= canvas.height / 2) { // i > 128
             if (j <= canvas.width / 2) { // j < 128
               positions.push({
-                x: (-this.planeWidth / 2) + (this.planeWidth / canvas.width) * j,
-                y: (i - canvas.width / 2) * (this.planeWidth / canvas.width)
+                x: (-this.planeWidth / 2) + ratio * j,
+                y: (i - canvas.width / 2) * ratio
               });
             } else { // j > 128
               positions.push({
-                x: (j - canvas.width / 2) * (this.planeWidth / canvas.width),
-                y: (i - canvas.width / 2) * (this.planeWidth / canvas.width)
+                x: (j - canvas.width / 2) * ratio,
+                y: (i - canvas.width / 2) * ratio
               });
             }
           }
@@ -168,34 +171,32 @@ export default class App {
 
       // Convert gray levels to z position (add to variable positions)
       for (let i = 0, c = positions.length; i < c; i++) {
-        positions[i].z  = pixels[i].red * (this.planeWidth / canvas.width);
+        positions[i].z  = pixels[i].red * ratio * scaleZ;
       }
 
-      // console.log(positions);
-
-      this.createTrees(positions); // draw Trees
+      this.createTrees(positions);
+      this.createStones(positions);
 
     }
 
-    // Place trees according to positions values calculate previously
+    /**
+      Place trees according to positions values calculate previously
+      */
     createTrees(positions) {
 
-      // console.log(positions);
-
       for (let i = 0; i < 60; i++) {
-        
+
         let trunkRadius = .75,
             trunkHeight = this.getRandom(3, 6),
             trunkRadiusSegments = this.getRandom(5, 8),
 
             // Get a random position in the Plane
             position = positions[this.getRandom(0, positions.length)],
-            zRating = 0.06,
 
             // Retrieve the position
-            trunkTreeX = position.x,
-            trunkTreeY = position.z * zRating,
-            trunkTreeZ = position.y,
+            treeX = position.x,
+            treeY = position.z,
+            treeZ = position.y,
 
             coneRadius = this.getRandom(3, 6),
             coneHeight = this.getRandom(10, 18),
@@ -213,10 +214,13 @@ export default class App {
           }
         );
 
+        this.trunkTreeMesh = new THREE.Mesh (trunkTree, trunkTreeMaterial);
+        this.trunkTreeMesh.position.set(treeX, treeY + trunkHeight / 2, treeZ);
+
         /**
           Cone
           */
-        let coneTree = new THREE.ConeGeometry(coneRadius, coneHeight, coneRadialSegments);
+        let coneTree = new THREE.ConeGeometry (coneRadius, coneHeight, coneRadialSegments);
         let coneTreeMaterial = new THREE.MeshPhongMaterial(
           {
             color: 0x2c714a,
@@ -226,17 +230,47 @@ export default class App {
           }
         );
 
-        // console.log(this);
-        this.trunkTreeMesh = new THREE.Mesh( trunkTree, trunkTreeMaterial );
         // console.log(this.trunkTreeMesh);
-        this.coneTreeMesh = new THREE.Mesh( coneTree, coneTreeMaterial );
-
-        this.trunkTreeMesh.position.set(trunkTreeX, trunkTreeY + trunkHeight / 2, trunkTreeZ);
-        this.coneTreeMesh.position.set(trunkTreeX, trunkTreeY + trunkHeight + coneHeight / 2, trunkTreeZ);
+        this.coneTreeMesh = new THREE.Mesh (coneTree, coneTreeMaterial);
+        this.coneTreeMesh.position.set(treeX, treeY + trunkHeight + coneHeight / 2, treeZ);
 
         this.scene.add( this.trunkTreeMesh, this.coneTreeMesh );
       }
 
+    }
+
+
+    /**
+      Stones
+      */
+
+    createStones(positions) {
+      for (let i = 0; i < 30; i++) {
+        let stroneRadius = this.getRandom(1, 4),
+
+          // Get a random position in the Plane
+          position = positions[this.getRandom(0, positions.length)],
+
+          // Retrieve the position
+          stoneX = position.x,
+          stoneY = position.z,
+          stoneZ = position.y;
+
+        let stone = new THREE.DodecahedronGeometry (stroneRadius);
+        let stoneMaterial = new THREE.MeshPhongMaterial(
+          {
+            color: 0x736b6b,
+            emissive: 0x524848,
+            specular: 0xa69f9f,
+            shininess: 5
+          }
+        );
+
+        this.stoneMesh = new THREE.Mesh (stone, stoneMaterial);
+        this.stoneMesh.position.set(stoneX, stoneY + stroneRadius, stoneZ);
+
+        this.scene.add( this.stoneMesh );
+      }
     }
 
     render() {
